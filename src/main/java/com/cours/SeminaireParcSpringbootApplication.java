ad1e01b7f52fbce683a5994a737b6e314afbe0db
unknown <Elmalik07>
2018-10-16 22:03:39 +0100

    Spring Boot

---
+++ src/main/java/com/cours/SeminaireParcSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.cours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminaireParcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminaireParcSpringbootApplication.class, args);
+	}
+}
